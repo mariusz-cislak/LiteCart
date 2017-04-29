781f6d9c5e28ab25d49bf4fbf86790802052cfe9
Mariusz <mariusz.cislak@gmail.com>
2017-04-29 16:33:14 +0200

    update Test2 - screenshots

--- src/test/java/org/tests/Tests.java
+++ src/test/java/org/tests/Tests.java
@@ -1,16 +1,23 @@
 package org.tests;
 
+import com.gargoylesoftware.htmlunit.javascript.host.intl.DateTimeFormat;
+import org.apache.commons.io.FileUtils;
+import org.apache.xerces.impl.dv.xs.DateTimeDV;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
@@ -42,6 +49,20 @@ public class Tests {
 //        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("hdtb-msb-vis")));
+        File screenShotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try {
+            Date dt = new Date();
+            String currDateTime = String.valueOf(dt.getSeconds())+"-"
+                    + String.valueOf(dt.getMinutes())+"-"
+                    + String.valueOf(dt.getHours())+"-"
+                    + String.valueOf(dt.getDate())+"-"
+                    + String.valueOf(dt.getMonth())+"-";
+            FileUtils.copyFile(screenShotFile, new File("D:\\SeleniumScreens\\" + currDateTime + ".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            driver.close();
+        }
     }
 
     @After
